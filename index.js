501c65d4a11f69d799f0ddd702e6344c43269f86
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-03-17 18:35:01 +0330

    implement template

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 3000);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
+
+module.exports = app
+
